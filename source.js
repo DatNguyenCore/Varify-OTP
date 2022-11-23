99678565fa898e5d5b834f637aaeb7b3d3d5cec3
DatNguyenCore <datnguyen2551998@gmail.com>
2022-11-23 17:56:31 +0700

    init project

---
+++ source.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const collection = document.getElementsByClassName("input-digit");
+
+  document.getElementById("form").onkeypress = function (e) {
+    var key = e.charCode || e.keyCode || 0;
+    if (key == 13) {
+      e.preventDefault();
+    }
+  };
+
+  document.getElementById("digit-1").focus();
+
+  for (let element of collection) {
+    element.addEventListener("keyup", (e) => {
+      const input = e.path[0];
+      console.log("values", e.keyCode);
+
+      if (e.keyCode === 32) {
+        // space
+        input.value = "";
+      } else if (e.keyCode === 8 || e.keyCode === 37) {
+        const classNamePrev = input.getAttribute("data-previous");
+        document.getElementById(classNamePrev).focus();
+      } else if (
+        (e.keyCode >= 48 && e.keyCode <= 57) ||
+        (e.keyCode >= 65 && e.keyCode <= 90) ||
+        (e.keyCode >= 96 && e.keyCode <= 105) ||
+        e.keyCode === 39
+      ) {
+        const classNameNext = input.getAttribute("data-next");
+
+        if (classNameNext === "submit-btn") {
+          document.getElementById(classNameNext).click();
+        } else {
+          document.getElementById(classNameNext).focus();
+        }
+      }
+    });
+  }
+});
